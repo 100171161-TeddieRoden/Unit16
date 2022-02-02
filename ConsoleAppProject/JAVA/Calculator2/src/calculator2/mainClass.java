f91adb4d4040cf0ba76e4e497cab36181bd9b91a
Teddie Roden <100171161@student.burnley.ac.uk>
2022-02-02 15:48:12 +0000

    Created Calculator2 Program

---
+++ ConsoleAppProject/JAVA/Calculator2/src/calculator2/mainClass.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculator2;
+
+/**
+ *
+ * @author 100171161
+ */
+public class mainClass {
+    public static void main(String[] args) {
+    
+    }
+}
