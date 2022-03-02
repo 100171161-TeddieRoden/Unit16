ab11b8fd835d69f90fd216db21dcf890c419a8f7
Teddie Roden <100171161@student.burnley.ac.uk>
2022-03-02 14:01:31 +0000

    Added code for input miles

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,13 +1,46 @@
-﻿namespace ConsoleAppProject.App01
+﻿using System;
+
+namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// A Distance converter
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Teddie Roden version 0.1
     /// </author>
     public class DistanceConverter
     {
-        
+        private double miles;
+
+        private double feet;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Run()
+        {
+            InputMiles();
+            CalculateFeet();
+            OutputFeet();
+        }
+        /// <summary>
+        /// prompt user to input miles
+        /// input miles as a double 
+        /// </summary>
+        private void InputMiles()
+        {
+            Console.Write("Please Enter The Number Of Miles > ")
+            string value = Console.ReadLine();
+            miles = Convert.ToDouble(value);
+        }
+        private void CalculateFeet()
+        {
+
+        }
+        private void OutputFeet()
+        {
+
+        }
+
     }
 }
