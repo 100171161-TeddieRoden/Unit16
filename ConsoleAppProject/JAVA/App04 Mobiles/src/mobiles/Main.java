101c528d99a5e76d40fc636691370d57ee450cd0
Teddie Roden <100171161@student.burnley.ac.uk>
2022-04-20 16:32:40 +0100

    Completed App04

--- ConsoleAppProject/JAVA/App04 Mobiles/src/mobiles/Main.java
+++ ConsoleAppProject/JAVA/App04 Mobiles/src/mobiles/Main.java
@@ -15,19 +15,47 @@ public class Main {
      */
     public static void main(String[] args) {
     Samsung s = new Samsung();
-    Nokia n = new Nokia();
-    Xiaomi x = new Xiaomi();
-    s.setColor("Black");
-    s.setCost(30000);
-    s.setRAM("4GB");
-    s.setandroid_version("Android Pie");
-    
+        Nokia n = new Nokia();
+        Xiaomi x = new Xiaomi();
+        
+        s.setandroid_version("Android Pie");
+        s.setColor("Black");
+        s.setCost(3000);
+        s.setRAM("4GB");
         System.out.println("Samsung mobile details");
         System.out.println("Cost: "+s.getCost());
-        System.out.println("Color: "+s.getColor());
+        System.out.println("Colour: "+s.getColor());
         System.out.println("RAM: "+s.getRAM());
         System.out.println("Android version: "+s.getandroid_version());
         
+        n.setColor("Silver");
+        n.setCost(40000);
+        n.setRAM("4GB");
+        n.setandroid_version("Andoid marshmelo");
+
+        x.setColor("Red");
+        x.setCost(50000);
+        x.setRAM("6GB");
+        x.setandroid_version("Andoid Kitkat");
+
+        System.out.println("Samsung Details: ");
+        System.out.println("Colour: "+s.getColor());
+        System.out.println("Cost: "+ s.getCost());
+        System.out.println("RAM"+ s.getRAM());
+        System.out.println("RAM: "+ s.getRAM());
+        System.out.println(" Samsung android version: " + s.getandroid_version());
+
+        System.out.println("Nokia Details: ");
+        System.out.println("Colour: "+n.getColor());
+        System.out.println("Cost: "+ n.getCost());
+        System.out.println("RAM: "+ n.getRAM());
+        System.out.println(" Nokia android version: " + n.getandroid_version());
+
+        System.out.println("Xiaomi Details: ");
+        System.out.println("Colour: "+x.getColor());
+        System.out.println("Cost: "+ x.getCost());
+        System.out.println("RAM: "+ x.getRAM());
+        System.out.println(" Xiaomi android version: " + x.getandroid_version());
     }
     
 }
